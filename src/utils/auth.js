a60bc2adb3fdc3da00504069cdca500670aa3219
msitabheer20 <msitabheerdey@gmail.com>
2023-10-12 10:22:56 +0530

    change .env

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -8,12 +8,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-      }),
-      GithubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET,
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
       }),
     ],
   }
