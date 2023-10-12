7f0cad980bf72888f60dcf71dfb66bc139b56d8f
msitabheer20 <msitabheerdey@gmail.com>
2023-10-12 11:51:09 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:[
+    images: {
+        domains: [
             "lh3.googleusercontent.com",
             "firebasestorage.googleapis.com"
         ]
+    },
+    env: {
+        NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+        NEXT_PUBLIC_GOOGLE_SECRET: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,NEXT_PUBLIC_FIREBASE: process.env.NEXT_PUBLIC_FIREBASE
     }
 }
 
