61621df85f371e89b9bfc50bd1a34835f504fef1
msitabheer20 <msitabheerdey@gmail.com>
2023-10-12 11:10:19 +0530

    debugging

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -80,7 +80,7 @@ const WritePage = () => {
       .replace(/^-+|-+$/g, "");
 
   const handleSubmit = async () => {
-    const res = await fetch("/api/posts", {
+    const res = await fetch("http://localhost:3000/api/posts", {
       method: "POST",
       body: JSON.stringify({
         title,
