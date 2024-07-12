b8b4c673e96693797cbd876b54446f3780a8cc7f
msitabheer20 <msitabheerdey@gmail.com>
2024-07-12 23:18:30 +0530

    fix category links

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -24,7 +24,7 @@ const CategoryList = async() => {
       <div className={styles.categories}>
         {data?.map(item => (
 
-        <Link href="/blog?cat=style" className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
+        <Link href={`/blog?cat=${item.slug}`} className={`${styles.category} ${styles[item.slug]}`} key={item._id}>
           {item.img && (<Image
             src={item.img}
             alt="" width={32}
--- src/components/themeToggle/themeToggle.jsx
+++ src/components/themeToggle/themeToggle.jsx
@@ -16,12 +16,14 @@ const ThemeToggle = () => {
       }
     >
       <Image src="/moon.png" alt="moon" width={14} height={14} />
+
       <div className={styles.ball} style={
         theme === "dark" ?
           { left:1, backgroundColor: "#0f172a" } :
           { right:1, backgroundColor: "white" }
       }
       ></div>
+      
       <Image src="/sun.png" alt="sun" width={14} height={14} />
     </div>
   )
